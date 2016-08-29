cf6698bb23327f2993811fd5e4a29aa135b9e55b
Michael Parlato <michaelvparlato@gmail.com>
2016-08-29 07:46:23 -0700

    Initial configuration - webpack, package.json, etc

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 module.exports = {
   context: __dirname,
-  entry: './frontend/pokedex.jsx',
+  entry: './frontend/iretain.jsx',
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
