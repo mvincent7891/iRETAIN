a59fd490565a543af8c8a9b1eb07cd8f7945acb3
Michael Parlato <michaelvparlato@gmail.com>
2016-09-09 12:50:44 -0700

    Chrome Extension works - words can be uploaded from other sites

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,18 +1,23 @@
 
+const local_path = "localhost:3000";
+const live_path = "www.iretain.io";
+const path = live_path;
+
 const retainIt = word => {
   var query = word.selectionText;
   var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:3000/api/cards/extension?word=" + query, false);
+  console.log(path);
+  xhr.open('POST', `http://${path}/api/cards/extension?word=` + query, false);
   xhr.send();
 };
 
 chrome.webRequest.onCompleted.addListener(function(details) {
   /* Process the XHR response */
   chrome.extension.getBackgroundPage().console.log(details);
-  if (details.url == "http://localhost:3000/session/new?error=MUST+LOGIN") {
-    chrome.tabs.create({url: "http://localhost:3000/session/new"});
+  if (details.url == `http://${path}/session/new?error=MUST+LOGIN`) {
+    chrome.tabs.create({url: s`http://${path}/session/new`});
   }
-}, {urls: ["http://localhost:3000/*"]});
+}, {urls: [`http://${path}/*`]});
 
 chrome.contextMenus.create({
   title: "Retain It",
