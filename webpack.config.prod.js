45b9ebbd63e3c95b07b89b834c794f8a4bf9b599
Michael Parlato <michaelvparlato@gmail.com>
2016-09-10 17:26:23 -0700

    post-build options

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './frontend/iretain.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
